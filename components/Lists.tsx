78a7acd8d0a548d606193daa4e3c52a6d5e847fa
Mayank <mayanksingh8181335335@gmail.com>
2025-01-02 09:09:43 +0530

    created list options in sidebar

--- components/Lists.tsx
+++ components/Lists.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import { useUserContext } from "@/context/UserContext";
 import React from "react";
 
 function Lists() {
-  return <div>Lists</div>;
+  const { userData } = useUserContext();
+
+  return (
+    <div>
+      <h1>Lists</h1>
+
+      <div className="mt-2 p-2 flex flex-col gap-1 text-black">
+        {userData &&
+          userData.lists.map((list, index) => (
+            <button
+              key={index}
+              className="bg-white p-1 hover:bg-gray-200 hover:font-semibold border-black border shadow-sm shadow-black"
+            >
+              {list}
+            </button>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default Lists;
