8a6d40b486fc142de70e47214603bc1585f78487
Mayank <mayanksingh8181335335@gmail.com>
2025-01-04 18:40:59 +0530

    made some chart updates

--- components/CategoryChart.tsx
+++ components/CategoryChart.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { PieChart } from "@mui/x-charts/PieChart";
+import { useUserContext } from "@/context/UserContext";
+import { useAuth } from "@/hooks/useAuth";
 
 function CategoryChart() {
+
+
+  const {user}=useAuth();
+
+  const {userData}=useUserContext();
+
+  useEffect(() => {
+if(user && userData)
+{
+  setData([]);
+  userData.lists.forEach((list,index)=>{
+    const value:number=userData.tasks.filter((task)=>task.list===list).length;
+
+    setData((prev)=>[...prev,{id:Date.now()+index,value,label:list}]);
+  })
+}
+  },[user,userData])
+
+  const [data,setData]=useState<{id:number,value:number,label:string}[]>([]);
+
+
+
   return (
-    <div>CategoryChart</div>
-  )
+    <div className="flex items-center justify-center h-full">
+      <PieChart className="h-full w-full"
+        series={[
+          {
+            data: data,
+            highlightScope: { fade: 'global', highlight: 'item' },
+            faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+          },
+        ]}
+      />
+    </div>
+  );
 }
 
-export default CategoryChart
+export default CategoryChart;
