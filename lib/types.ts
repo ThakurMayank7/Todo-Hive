47177bd4a0ddfc22445fb377ea7f8fc3df7837bc
Mayank <mayanksingh8181335335@gmail.com>
2025-01-08 00:04:13 +0530

    made some changes

--- lib/types.ts
+++ lib/types.ts
@@ -1,18 +1,18 @@
 export interface Task {
-    uid: string;
-    taskName: string;
-    taskDescription?: string;
-    list: string;
-    dueDate: Date;
-    tags?: string[];
-    subTasks?: { sTask: string; sStatus: boolean }[];
-    status: boolean;
-    createdAt: Date;
-    taskId: string;
-  }
-  
-  export interface UserData {
-    tasks: Task[];
-    lists: string[];
-    tags: string[];
-  }
+  uid: string;
+  taskName: string;
+  taskDescription?: string;
+  list: string;
+  dueDate: Date;
+  tags?: string[];
+  subTasks?: { sTask: string; sStatus: boolean }[];
+  status: boolean;
+  createdAt: Date;
+  taskId: string;
+}
+
+export interface UserData {
+  tasks: Task[];
+  lists: string[];
+  tags: string[];
+}
